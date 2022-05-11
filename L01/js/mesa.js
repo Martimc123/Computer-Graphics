c9ab3fd64e842b515f891bca2dc514bf1e536b47
Alexisminas <alexisminas308@gmail.com>
2022-05-11 13:28:59 +0100

    Finished Lab01, now ball moves and table is a prettier color

--- L01/js/mesa.js
+++ L01/js/mesa.js
@@ -4,6 +4,25 @@ var camera, scene, renderer;
 
 var geometry, material, mesh;
 
+var ball;
+
+function createBall(x, y, z) {
+ 'use strict';
+
+ ball = new THREE.Object3D();
+ ball.userData = { jumping: true, step:0 };
+
+ material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+ geometry = new THREE.SphereGeometry(4, 10, 10);
+ mesh = new THREE.Mesh(geometry, material);
+
+ ball.add(mesh);
+ ball.position.set(x, y, z);
+
+ scene.add(ball);
+}
+
+
 function addTableTop(obj, x, y, z) {
 	'use strict';
 	geometry = new THREE.BoxGeometry(60, 2, 20);
@@ -25,7 +44,7 @@ function createTable(x, y, z) {
 
 	var table = new THREE.Object3D();
 
-	material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true});
+	material = new THREE.MeshBasicMaterial({ color: 0x20b2aa, wireframe: true});
 
 	addTableTop(table, 0, 0, 0);
 	addTableLeg(table, -25 ,-1, -8);
@@ -45,11 +64,58 @@ function render() {
 	renderer.render(scene, camera); // tells 3js renderer to draw scene visualization based on camera 
 }
 
+function onResize() {
+	'use strict';
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	if (window.innerWidth > 0 &&  window.innerHeight > 0) {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	}
+
+}
+
+function onKeyDown(e) {
+	'use strict';
+
+	switch(e.keyCode) {
+		case 65: //A
+		case 97: //a
+			scene.traverse( function(node) {
+				if (node instanceof THREE.Mesh) {
+					node.material.wireframe = !node.material.wireframe;
+				}
+			});
+			break;
+		case 83: // S
+		case 115: //s
+			ball.userData.jumping = !ball.userData.jumping;
+			break;
+	}
+
+}
+
+function animate() {
+	'use strict';
+
+	if (ball.userData.jumping) {
+		ball.userData.step += 0.04;
+		ball.position.y = Math.abs(30*Math.sin(ball.userData.step));
+		ball.position.z = 15 * (Math.cos(ball.userData.step));
+	}
+
+	render();
+
+	requestAnimationFrame(animate);
+}
+
 function createScene() {
 	'use strict';
 	scene = new THREE.Scene();
-	scene.add(new THREE.AxesHelper(10));
+	//scene.add(new THREE.AxesHelper(10)); !!
 	createTable(0, 0, 0);
+	createBall(0, 0, 15);
 }
 
 function createCamera() {
@@ -80,4 +146,7 @@ function init() {
 	createCamera();
 
 	render();
+
+	window.addEventListener("resize", onResize);
+	window.addEventListener("keydown", onKeyDown);
 }
