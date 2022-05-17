abce71e84d21b1544359ad1f43c53c15e71115f3
Martimc <martim.correia123@hotmail.com>
2022-05-17 12:32:31 +0100

    added cameras and wire keys

--- Project/js/kitty2.js
+++ Project/js/kitty2.js
@@ -1,149 +1,154 @@
 /*global THREE*/
 
-var camera, scene, renderer;
+"use strict";
+
+var camera = [], scene, renderer, currentCamera = 0;
+
+var viewSize = 50;
+var aspectRatio;
 
 var geometry, material, mesh;
 
 var kitty;
 var head;
+var wires;
 
-var body;
-var tail;
-var face;
-var ear_left;
-var ear_right;
-var nose;
-var wisker_left_top;
-var wisker_left_bottom;
-var wisker_right_top;
-var wisker_right_bottom;
-var leg_front_right;
-var leg_front_left;
-var leg_back_right;
-var leg_back_left;
+var cat_body;
+var cat_face;
 
+//const controls = new OrbitControls( camera[currentCamera], renderer.domElement );
 
 // change to receive position, yes, but also body radius and height
 function createKitty(x, y, z) {
-	'use strict';
-	
-	var kitty = new THREE.Object3D();
+    
+    wires = true;
+    kitty = new THREE.Object3D();
 
-	// change for positions to be based on cylinder height and angle
-	addBody(kitty, 0, 0, 0); // based on kitty origin position
-	addFace(kitty, 6.5, 3, 0);
+    // change for positions to be based on cylinder height and angle
+    addBody(kitty, 0, 0, 0); // based on kitty origin position
+    addFace(kitty, 6.5, 3, 0);
 
-	kitty.position.set(x, y, z);
+    kitty.position.set(x, y, z);
 
-	scene.add(kitty);
+    scene.add(kitty);
+	return kitty;
 }
 
 function addBody(obj, x, y, z) {
-	'use strict';
-	geometry = new THREE.CylinderGeometry(2, 2, 8, 20, 1);
-	material = new THREE.MeshBasicMaterial({ color: 0x35e8df, wireframe: false});
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
-	mesh.rotateZ( Math.PI / 180 * 90);
-	obj.add(mesh);
+    geometry = new THREE.CylinderGeometry(2, 2, 8, 20, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0x35e8df, wireframe: wires});
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    mesh.rotateZ( Math.PI / 180 * 90);
+    mesh.name="body";
+    obj.add(mesh);
 }
 
 function addFace(obj, x, y, z) {
-	'use strict';
-	geometry = new THREE.SphereGeometry(2);
-	material = new THREE.MeshBasicMaterial({ color: 0xfde995, wireframe: false});
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
-	obj.add(mesh);
-}
-
-function addEye(obj, x, y, z) {
-	'use strict';
-	geometry = new THREE.SphereGeometry(0.25);
-	material = new THREE.MeshBasicMaterial({ color: 0x49b517, wireframe: false});
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
-	obj.add(mesh);
+    geometry = new THREE.SphereGeometry(2);
+    material = new THREE.MeshBasicMaterial({ color: 0xfde995, wireframe: wires});
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    mesh.name="face";
+    obj.add(mesh);
 }
 
 function render() {
-	'use strict'; // strict mode makes for faster code and supresses some warnings
-	renderer.render(scene, camera); // tells 3js renderer to draw scene visualization based on camera 
+    renderer.render(scene, camera[currentCamera]); // tells 3js renderer to draw scene visualization based on camera 
 }
 
 function onResize() {
-	'use strict';
 
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	if (window.innerWidth > 0 &&  window.innerHeight > 0) {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	}
+    var val = 2;
+    var newAspectRatio = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    camera[currentCamera].left = -viewSize * newAspectRatio / val;
+    camera[currentCamera].right = viewSize * newAspectRatio / val;
+    camera[currentCamera].top = viewSize / val;
+    camera[currentCamera].bottom = viewSize / -val;
+    camera[currentCamera].updateProjectionMatrix();
+       
+    aspectRatio = window.innerWidth / innerHeight;
 
 }
 
-function onKeyDown(e) {
-	'use strict';
+function keys()
+{
+	document.addEventListener ('keypress', (event) => {
+        const keyName = event.key;
+        switch (keyName) {
+            case "1":
+                currentCamera = 0;
+            break;
 
-	switch(e.keyCode) {
-		case 65: //A
-		case 97: //a
-			scene.traverse( function(node) {
-				if (node instanceof THREE.Mesh) {
-					node.material.wireframe = !node.material.wireframe;
+            case "2":
+                currentCamera = 1;
+            break;
+
+            case "3":
+                currentCamera = 2;
+            break;
+
+			case "4":
+				if (wires == true)
+				{
+					wires = false;
+					kitty.getObjectByName("body").material = new THREE.MeshBasicMaterial({ color: 0x35e8df, wireframe: wires});
+					kitty.getObjectByName("face").material = new THREE.MeshBasicMaterial({ color: 0xfde995, wireframe: wires});
 				}
-			});
-			break;
-		case 83: // S
-		case 115: //s
-			ball.userData.jumping = !ball.userData.jumping;
-			break;
-	}
-
+				else
+				{
+					wires = true;
+					kitty.getObjectByName("body").material = new THREE.MeshBasicMaterial({ color: 0x35e8df, wireframe: wires});
+					kitty.getObjectByName("face").material = new THREE.MeshBasicMaterial({ color: 0xfde995, wireframe: wires});
+				}		
+				break;
+			default:
+				break;
+		}
+    });   
 }
 
 function animate() {
-	'use strict';
-	render();
-
-	requestAnimationFrame(animate);
+    
+    render();
+    requestAnimationFrame(animate);
+    //controls.update();
 }
 
 function createScene() {
-	'use strict';
-	scene = new THREE.Scene();
-	scene.add(new THREE.AxesHelper(100));
-	createKitty(4, 5, 2);
+    scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(100));
+    kitty = createKitty(4, 5, 2);
 }
 
-function createCamera() {
-	'use strict';
-
-	// FOV=70, Aspect ratio, ETX
-	// scene could have ortogonal camera instead but its not the case
-	camera = new THREE.PerspectiveCamera(70,
-																		window.innerWidth/window.innerHeight,
-																		1,
-																		1000);
-	camera.position.x = 0;
-	camera.position.y = 0;
-	camera.position.z = 50;
-	camera.lookAt(scene.position); // camera pointed at scene axis
+function createCamera(x, y, z) {
+    var val = 2;
+    aspectRatio = window.innerWidth / window.innerHeight;
+    var camera = new THREE.OrthographicCamera( viewSize * aspectRatio/-val, 
+                                            viewSize * aspectRatio / val, 
+                                            viewSize / val, 
+                                            viewSize / -val, 
+                                            1, 
+                                            1000);
+    camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
+    camera.lookAt(scene.position);
+    
+    return camera;
 }
 
 function init() {
-	'use strict';
-
-	renderer = new THREE.WebGLRenderer({antialias: true}); // antialias softens pixel transition when changing image size
-
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	document.body.appendChild(renderer.domElement); // associates html to renderer
-	createScene();
-	createCamera();
-
-	render();
-	window.addEventListener("resize", onResize);
-
+    
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+   
+    createScene();
+    camera[0] = createCamera(50, 0, 0);
+    camera[1] = createCamera(0, 50, 0);
+    camera[2] = createCamera(0, 0, 50);
+    window.addEventListener("resize", onResize);
 }
