bfa902ac304426262ee40a43a3a101719dc0a938
Alexisminas <alexisminas308@gmail.com>
2022-06-03 21:56:39 +0100

    New movement mechanics

--- Project/P2/js/rocket.js
+++ Project/P2/js/rocket.js
@@ -1,5 +1,8 @@
-// To generate the images, the videos and the overall project, run the command " python -m http.server "
-// in a terminal in the index.html folder and search for the url localhost:8000 in a browser
+// To generate the images, the videos and the overall project:
+//  1. Run the command "python -m http.server" on a terminal in the index.html folder
+//  2. Search for the url "localhost:8000" in a browser
+// It may take a few seconds to load (about a minute, in the first try).
+// For the Ubuntu wsl terminal, use "python -m SimpleHTTPServer 8000" instead
 
 /*global THREE*/
 var camera = [];
@@ -14,11 +17,9 @@ var wiredObjects = [];
 var leftArrow, rightArrow, upArrow, downArrow;
 var clock = new THREE.Clock();
 
-var controls;
-
 var defaultScale = 1;
 var planetRadius = 12;
-var rocketHeight = planetRadius/11;
+var rocketHeight = planetRadius/12;
 var rocketPartHeight = rocketHeight/2;
 var rocketInfRadius = rocketPartHeight;
 var rocketMidRadius = rocketPartHeight/2;
@@ -30,8 +31,9 @@ var objPositions = [];
 var objAngles = [];
 var nrTrash = 20;
 var markedTrash = [];
-var minTrashSize = planetRadius/24;
-var maxTrashSize = planetRadius/20;
+var trashLen = planetRadius/24;
+var trashRadius = trashLen/2;
+var rocketBoundaryRadius = Math.sqrt((rocketPartHeight+boosterHeight)**2 + rocketInfRadius**2);
 var quadrants = [[],[],[],[],[],[],[],[],[]]; // case in border of quadrants goes to 9
 
 var copyVideo;
@@ -100,17 +102,17 @@ function createUniverse(x, y, z, scale) {
 
 	addPlanet(universe, 0, 0, 0);
 	addRocket(universe, rocketPos.x, rocketPos.y, rocketPos.z);
-	addAux(universe);
+	rocket.lookAt(scene.position);
 
 	universe.add(trash);
-	GenerateTrash(trash);
+	generateTrash(trash);
 
 	universe.position.set(x, y, z);
 	scene.add(universe);
 	return universe;
 }
 
-function HasColision(obj1,obj2)
+function hasColision(obj1,obj2)
 {
 	radius = (obj1.radius+obj2.radius)**2;
 	distance = (obj1.center.x-obj2.center.x)**2 + (obj1.center.y-obj2.center.y)**2 + (obj1.center.z-obj2.center.z)**2;
@@ -121,19 +123,20 @@ function getRndInteger(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function GenerateTrash(obj)
+function generateTrash(obj)
 {
-	for (i = 1;i<21;i++)
+	var nrObj = nrTrash+1;
+	for (i = 1;i<nrObj;i++)
 	{
 		var option = getRndInteger(1,4);
 		if (option == 1)
 		{
 			cubic_trash = new THREE.Object3D();
-			geometry = new THREE.BoxGeometry( (planetRadius/21)/2, (planetRadius/21)/2, (planetRadius/21)/2 );
+			geometry = new THREE.BoxGeometry(trashLen, trashLen, trashLen);
 			material = new THREE.MeshBasicMaterial( {map: compressed_trash_texture,transparent:true} );
 			addObjPart(cubic_trash, geometry, material, 0x00ff00, objPositions[i].x,objPositions[i].y,objPositions[i].z,0,0,0,tag = "");
-			addBoundaryBox(cubic_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z,(planetRadius/21));
-			cubic_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),1.5);
+			addBoundary(cubic_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z, Math.sqrt(2*trashRadius**2));
+			cubic_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),Math.sqrt(2*trashRadius**2));
 			cubic_trash.name = "trash"+i;
 			quadrants[quadrantIdentifier(objPositions[i].x,objPositions[i].y,objPositions[i].z)-1].push(cubic_trash);
 			obj.add(cubic_trash);
@@ -141,11 +144,11 @@ function GenerateTrash(obj)
 		if (option == 2)
 		{
 			polyhedron_trash = new THREE.Object3D();
-			geometry = new THREE.DodecahedronGeometry((planetRadius/21)/2,0);
+			geometry = new THREE.DodecahedronGeometry(trashRadius,0);
 			material = new THREE.MeshBasicMaterial( {map: compressed_trash_texture,transparent:true} );
 			addObjPart(polyhedron_trash, geometry, material, 0x00ff00, objPositions[i].x,objPositions[i].y,objPositions[i].z,0,0,0,tag = "");
-			addBoundaryBox(polyhedron_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z,planetRadius/21+1);
-			polyhedron_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),1.5);
+			addBoundary(polyhedron_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z,trashRadius);
+			polyhedron_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),trashRadius);
 			polyhedron_trash.name = "trash"+i;
 			quadrants[quadrantIdentifier(objPositions[i].x,objPositions[i].y,objPositions[i].z)-1].push(polyhedron_trash);
 			obj.add(polyhedron_trash);
@@ -153,11 +156,11 @@ function GenerateTrash(obj)
 		if (option == 3)
 		{
 			cone_trash = new THREE.Object3D();
-			geometry = new THREE.ConeGeometry( (planetRadius/21)/2, 1, 11 );
+			geometry = new THREE.ConeGeometry(trashRadius, 2*trashRadius, 11 );
 			material = new THREE.MeshBasicMaterial( {map: compressed_trash_texture,transparent:true} );
 			addObjPart(cone_trash, geometry, material, 0x00ff00, objPositions[i].x,objPositions[i].y,objPositions[i].z,0,0,0,tag = "");
-			addBoundaryBox(cone_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z,planetRadius/21+0.2);
-			cone_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),1.5);
+			addBoundary(cone_trash,objPositions[i].x,objPositions[i].y,objPositions[i].z,Math.sqrt(2*trashRadius**2));
+			cone_trash.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(objPositions[i].x,objPositions[i].y,objPositions[i].z),Math.sqrt(2*trashRadius**2));
 			cone_trash.name = "trash"+i;
 			quadrants[quadrantIdentifier(objPositions[i].x,objPositions[i].y,objPositions[i].z)-1].push(cone_trash);
 			obj.add(cone_trash);
@@ -169,7 +172,7 @@ function checkCollisions()
 {
 	var i = quadrantIdentifier(objPositions[0].x,objPositions[0].y,objPositions[0].z)-1;
 	quadrants[i].forEach( object => { 
-		if (HasColision(rocket.getObjectByName("bb").geometry.boundingSphere,object.getObjectByName("bb").geometry.boundingSphere))
+		if (hasColision(rocket.getObjectByName("bb").geometry.boundingSphere,object.getObjectByName("bb").geometry.boundingSphere))
 		{
 			markedTrash.push(object);
 		}
@@ -236,7 +239,7 @@ function addRocket(obj, x, y, z) {
 	addRocketBooster(rocket, -rocketInfRadius+boosterRadius, -rocketPartHeight-0.5*boosterHeight, 0);
 	addRocketBooster(rocket, 0,-rocketPartHeight-0.5*boosterHeight, rocketInfRadius-boosterRadius);
 	addRocketBooster(rocket, 0,-rocketPartHeight-0.5*boosterHeight, -rocketInfRadius+boosterRadius);
-	addBoundaryBox(rocket,0, 0, rocketPartHeight/2,rocketHeight);
+	addBoundary(rocket,0, 0, 0, rocketBoundaryRadius);
 	rocket.rotation.z = -Math.PI/180 * 90;
 	rocket.position.set(x, y, z);
 	obj.add(rocket);
@@ -244,12 +247,12 @@ function addRocket(obj, x, y, z) {
 }
 
 
-function addBoundaryBox(obj,x,y,z,raio)
+function addBoundary(obj,x,y,z,radius)
 {
-	geometry = new THREE.SphereGeometry(raio);
-	geometry.boundingSphere = new THREE.Sphere(raio);
+	geometry = new THREE.SphereGeometry(radius);
+	geometry.boundingSphere = new THREE.Sphere(radius);
 	geometry.boundingSphere.center = new THREE.Vector3(x,y,z);
-	geometry.boundingSphere.radius = 1.5;
+	geometry.boundingSphere.radius = radius;
 	material = {};
 	addObjPart(obj, geometry, material, 0xffff00, x,y,z, 0,0,0,"bb");
 }
@@ -304,21 +307,8 @@ function changeWires(wires) {
 	}
 }
 
-function myLookAt(obj, targetPosition) {
-	var targetPos = obj.worldToLocal(targetPosition.clone());
-    var rotationAxis = new THREE.Vector3().crossVectors(
-        new THREE.Vector3(0, 0, -1),
-        targetPos
-    ).normalize();
-    var angle = new THREE.Vector3(0, 0, -1).angleTo(
-        targetPos.normalize().clone());
-
-    obj.rotateOnAxis(rotationAxis, angle);
-}
-
 function update()
 {
-	controls.update();
 	var timeOccurred = clock.getDelta();
 	var rocketSpeed = Math.PI/180 * 40;
 	var degreesTraveled = rocketSpeed*timeOccurred;
@@ -331,39 +321,51 @@ function update()
 		var rocketX, rocketY, rocketZ;
 		var rotZ;
 		
-		if (leftArrow){	
-			rocketPhi += dirLongitudeCoef * degreesTraveled;
-			rotZ = -Math.PI/2;
+		// Nullifies opposite direction arrows for simplicity
+		if (leftArrow && rightArrow) {
+			leftArrow=false;
+			rightArrow = false;
 		}
-		if (rightArrow){
-			rocketPhi += -dirLongitudeCoef * degreesTraveled;
-			rotZ = Math.PI/2;
-		}
-		if (upArrow){
-			rocketTheta += dirLatitudeCoef * degreesTraveled;
-			rotZ = Math.PI;
-		}
-		if (downArrow){
-			rocketTheta += - dirLatitudeCoef * degreesTraveled;
-			rotZ = -Math.PI;
+		if (upArrow && downArrow) {
+			upArrow = false;
+			downArrow = false;
 		}
 
-		// If speed is in both directions, to mantain total, speed in each direction is halved
 		if (leftArrow && upArrow) {
-			rocketPhi += -dirLongitudeCoef * degreesTraveled /2;
-			rocketTheta += -dirLatitudeCoef * degreesTraveled /2;
+			rotZ = -Math.PI/4;
+			rocketPhi += dirLongitudeCoef * Math.sqrt(0.5)*degreesTraveled;
+			rocketTheta += dirLatitudeCoef * Math.sqrt(0.5)*degreesTraveled;
 		}
-		if (leftArrow && downArrow) {
-			rocketPhi += -dirLongitudeCoef * degreesTraveled /2;
-			rocketTheta += dirLatitudeCoef * degreesTraveled /2;
+		else if (leftArrow && downArrow) {
+			rotZ = -3*Math.PI/4;
+			rocketPhi += dirLongitudeCoef * Math.sqrt(0.5)*degreesTraveled;
+			rocketTheta += - dirLatitudeCoef * Math.sqrt(0.5)*degreesTraveled;
 		}
-		if (rightArrow && upArrow) {
-			rocketPhi += dirLongitudeCoef * degreesTraveled /2;
-			rocketTheta += -dirLatitudeCoef * degreesTraveled /2;
+		else if (rightArrow && upArrow) {
+			rotZ = Math.PI/4;
+			rocketPhi += -dirLongitudeCoef * Math.sqrt(0.5)*degreesTraveled;
+			rocketTheta += dirLatitudeCoef * Math.sqrt(0.5)*degreesTraveled;
 		}
-		if (rightArrow && downArrow) {
-			rocketPhi += dirLongitudeCoef * degreesTraveled /2;
-			rocketTheta += dirLatitudeCoef * degreesTraveled /2;
+		else if (rightArrow && downArrow) {
+			rotZ = 3*Math.PI/4;
+			rocketPhi += -dirLongitudeCoef * Math.sqrt(0.5)*degreesTraveled;
+			rocketTheta += - dirLatitudeCoef * Math.sqrt(0.5)*degreesTraveled;
+		}
+		else if (leftArrow) {
+			rotZ = -Math.PI/2;
+			rocketPhi += dirLongitudeCoef * degreesTraveled;
+		}
+		else if (rightArrow) {
+			rotZ = Math.PI/2;
+			rocketPhi += -dirLongitudeCoef * degreesTraveled;
+		}
+		else if (upArrow) {
+			rotZ = 0;
+			rocketTheta += dirLatitudeCoef * degreesTraveled;
+		}
+		else if (downArrow) {
+			rotZ = Math.PI;
+			rocketTheta += - dirLatitudeCoef * degreesTraveled;
 		}
 		
 		rocketX = rocketTrashDistance * Math.sin(rocketTheta) * Math.sin(rocketPhi);
@@ -374,7 +376,7 @@ function update()
 		rocket.lookAt(scene.position);
 		objAngles[0].set(rocketTheta, rocketPhi);
 		objPositions[0].set(rocketX, rocketY, rocketZ);
-		rocket.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(rocketX, rocketY, rocketZ),1.5);
+		rocket.getObjectByName("bb").geometry.boundingSphere.set(new THREE.Vector3(rocketX, rocketY, rocketZ),rocketBoundaryRadius);
 		checkCollisions();
 	}
 }
@@ -389,13 +391,6 @@ function display() {
 function animate() {
 	update();
 	display();
-}
-
-function addAux(obj) {
-	geometry = new THREE.SphereGeometry(5);
-	addObjPart(obj, geometry, null, 0xffc0cb, 15, 0, 0);
-	addObjPart(obj, geometry, null, 0xffff00, 15, 0, 0);
-	addObjPart(obj, geometry, null, 0x0000ff, 15, 0, 0);
 }
 
 function createScene() {
@@ -452,9 +447,6 @@ function onKeyDown(e) {
 		case 51://3
 			currentCamera = 2;
 			break;
-		case 53: //5
-			currentCamera = 3;
-			break;
 			
 		case 52://4
 			wires = !wires;
@@ -508,10 +500,8 @@ function init() {
 	camera[0] = createOrtographicCamera(0, 0, viewSize);
 	camera[1] = createPerspectiveCamera(viewSize/2, viewSize/2, viewSize/2);
 	camera[2] = createPerspectiveCamera(0, -rocketInfRadius*2.5, -rocketPartHeight*3);
-	camera[3] = createOrtographicCamera(0, 0, viewSize);
 	rocket.add(camera[2]);
 	rocket.add(new THREE.AxesHelper(10));
-	controls = new THREE.OrbitControls(camera[currentCamera], renderer.domElement);
 	animate();
 	video.addEventListener("playing", function() {
 		copyVideo = true;
